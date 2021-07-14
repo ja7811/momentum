28a2b9cfdae107f4837f69c83fbb099513c8e2bd
JaehwanH <jec0101@naver.com>
2021-07-14 20:02:48 +0900

    배경화면용 이미지 업로드

--- js/background.js
+++ js/background.js
@@ -9,11 +9,55 @@ const images = [
     "7.jpg"
 ]
 
+const clearImages = [
+    "clear0.jpg",
+    "clear1.jpg",
+    "clear2.jpg",
+    "clear3.jpg",
+    "clear4.jpg"
+]
+
+const cloudImages = [
+    "cloud0.jpg",
+    "cloud1.jpg",
+    "cloud2.jpg",
+    "cloud3.jpg",
+    "cloud4.jpg"
+]
+
+const extremeImages = [
+    "extreme0.jpg",
+    "extreme1.jpg",
+    "extreme2.jpg"
+]
+
+const hotImages = [
+    "hot0.jpg",
+    "hot1.jpg",
+    "hot2.jpg",
+    "hot3.jpg"
+]
+
+const rainImages = [
+    "rain0.jpg",
+    "rain1.jpg",
+    "rain2.jpg",
+    "rain3.jpg"
+]
+
+const snowImages = [
+    "snow0.jpg",
+    "snow1.jpg",
+    "snow2.jpg",
+    "snow3.jpg"
+]
+
 const randomIdx = Math.floor(Math.random() * images.length);
 const img = document.createElement("img");
 img.classList.add("background");
 img.src = `img/default/${images[randomIdx]}`;
 document.body.appendChild(img);
+console.log("background.js : ", weather);
 
 // Clouds
 // Rain
