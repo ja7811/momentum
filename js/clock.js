12374f5d4b828dce38cf7da873af0f75c59ca5a4
JaehwanH <jec0101@naver.com>
2021-07-14 14:52:32 +0900

    seconds 변수 삭제

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,6 @@ function updateClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerHTML = `${hours}:${minutes}`;
 }
 
